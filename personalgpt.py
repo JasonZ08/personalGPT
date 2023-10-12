cf0c5a0bf01f62c4d3c3321b64c2c4d0ab8164d8
Jason Zhong <jasonzhong0408@gmail.com>
2023-10-12 18:42:25 -0400

    cleanup

--- personalgpt.py
+++ personalgpt.py
@@ -9,6 +9,37 @@ from langchain.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 import sqlite3
 
+def execute_sql_command(statement):
+
+    #connect to database and execute the command
+    conn = sqlite3.connect("./database/calendar.db")
+    cur = conn.cursor()
+
+    try:
+        cur.execute(statement)
+    except:
+        print("There is something wrong with your SQL insert statement")
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def query_txt(query):
+    loader = DirectoryLoader("./data", glob="*.txt")
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+    return index.query(query, llm=ChatOpenAI())
+
+def query_db(query):
+
+    #connect to the database
+    db = SQLDatabase.from_uri("sqlite:///database/calendar.db")
+    llm = ChatOpenAI()
+    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+
+    #run the query
+    return db_chain.run(query)
+
 def main():
 
     #OpenAI API Key Configuration
@@ -20,36 +51,12 @@ def main():
     colon_index = query.find(":")
     if colon_index != -1 and query[:colon_index] == "SQLITEINPUT":
 
-        conn = sqlite3.connect("./database/calendar.db")
-        cur = conn.cursor()
-
-        try:
-            statement = query[colon_index+1:].strip()
-            cur.execute(statement)
-        except:
-            print("There is something wrong with your SQL insert statement")
-
-        conn.commit()
-        cur.close()
-        conn.close()
+        #call the execute sql function
+        execute_sql_command(query[colon_index+1:].strip())
     else:
-        """
 
-        query = sys.argv[1]
-        print(query) 
-
-        loader = DirectoryLoader("./data", glob="*.txt")
-        index = VectorstoreIndexCreator().from_loaders([loader])
-
-        print(index.query(query, llm=ChatOpenAI()))
-        """
-        #connect to the database
-        db = SQLDatabase.from_uri("sqlite:///database/calendar.db")
-        llm = ChatOpenAI()
-        db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
-
-        #run the query
-        answer = db_chain.run(query)
+        #Call query database or txt file and print it
+        answer = query_db(query)
         print(answer)
 
 if __name__ == "__main__":
